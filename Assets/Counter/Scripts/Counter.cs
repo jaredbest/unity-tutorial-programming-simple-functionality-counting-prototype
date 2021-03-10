8aefbc9a0b0d919cc6736364c3268725b61f8822
jaredbest <me@jaredbest.com>
2021-03-10 14:29:28 +0100

    Finished tutorial

---
+++ Assets/Counter/Scripts/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    public TextMeshProUGUI counterText;
+    int count = 0;
+    int whiskeyCount = 0;
+    int lemonJuiceCount = 0;
+    int sugarSyrupCount = 0;
+
+    public TextMeshProUGUI cocktailDisplayText;
+
+    void Start()
+    {
+        count = 0;
+        whiskeyCount = 0;
+        lemonJuiceCount = 0;
+        sugarSyrupCount = 0;
+        cocktailDisplayText.enabled = false;
+
+        counterText.text = "Number of ingredients: " + count + "\n" + "Whiskey: " + whiskeyCount + "\n" + "Lemon juice: " + lemonJuiceCount + "\n" + "Sugar syrup: " + sugarSyrupCount;
+    }
+
+    void Update()
+    {
+        if (whiskeyCount == 1 && lemonJuiceCount == 1 && sugarSyrupCount == 1)
+            cocktailDisplayText.enabled = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        count += 1;
+
+        if (other.CompareTag("Whiskey"))
+            whiskeyCount += 1;
+
+        if (other.CompareTag("Lemon Juice"))
+            lemonJuiceCount += 1;
+
+        if (other.CompareTag("Sugar Syrup"))
+            sugarSyrupCount += 1;
+
+        counterText.text = "Number of ingredients: " + count + "\n" + "Whiskey: " + whiskeyCount + "\n" + "Lemon juice: " + lemonJuiceCount + "\n" + "Sugar syrup: " + sugarSyrupCount;
+    }
+}
